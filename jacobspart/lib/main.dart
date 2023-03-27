c0b5231246adf2c961e94e3561ed30fb08fd8403
Jacob2740 <71669868+Jacob2740@users.noreply.github.com>
2023-03-26 21:44:32 -0400

    Second Commit

--- jacobspart/lib/main.dart
+++ jacobspart/lib/main.dart
@@ -1,20 +1,82 @@
 import 'package:flutter/material.dart';
 
+// function to trigger build when the app is run
 void main() {
-  runApp(const MainApp());
+runApp(MaterialApp(
+	initialRoute: '/',
+	routes: {
+	'/': (context) => const HomeRoute(),
+	'/second': (context) => const SecondRoute(),
+	'/third': (context) => const ThirdRoute(),
+	},
+)); //MaterialApp
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class HomeRoute extends StatelessWidget {
+const HomeRoute({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+@override
+Widget build(BuildContext context) {
+	return Scaffold(
+	appBar: AppBar(
+		title: const Text('Geeks for Geeks'),
+		backgroundColor: Colors.green,
+	), // AppBar
+	body: Center(
+		child: Column(
+		mainAxisAlignment: MainAxisAlignment.center,
+		children: <Widget>[
+			ElevatedButton(
+			child: const Text('Click Me!'),
+			onPressed: () {
+				Navigator.pushNamed(context, '/second');
+			},
+			), // ElevatedButton
+			ElevatedButton(
+			child: const Text('Tap Me!'),
+			onPressed: () {
+				Navigator.pushNamed(context, '/third');
+			},
+			), // ElevatedButton
+		], // <Widget>[]
+		), // Column
+	), // Center
+	); // Scaffold
+}
+}
+
+class SecondRoute extends StatelessWidget {
+const SecondRoute({Key? key}) : super(key: key);
+
+@override
+Widget build(BuildContext context) {
+	return Scaffold(
+	appBar: AppBar(
+		title: const Text("Click Me Page"),
+		backgroundColor: Colors.green,
+	), // AppBar
+	body: Center(
+		child: ElevatedButton(
+		onPressed: () {
+			Navigator.pop(context);
+		},
+		child: const Text('Back!'),
+		), // ElevatedButton
+	), // Center
+	); // Scaffold
+}
+}
+
+class ThirdRoute extends StatelessWidget {
+const ThirdRoute({Key? key}) : super(key: key);
+
+@override
+Widget build(BuildContext context) {
+	return Scaffold(
+	appBar: AppBar(
+		title: const Text("Tap Me Page"),
+		backgroundColor: Colors.green,
+	), // AppBar
+	); // Scaffold
+}
 }

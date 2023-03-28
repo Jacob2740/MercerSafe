94cc42f65824f24843086008e6799e5d3df5c308
Jacob2740 <71669868+Jacob2740@users.noreply.github.com>
2023-03-28 00:17:24 -0400

    9th Commit

--- jacobspart/lib/main.dart
+++ jacobspart/lib/main.dart
@@ -3,102 +3,226 @@ import 'package:url_launcher/url_launcher.dart';
 
 // function to trigger build when the app is run
 void main() {
-  runApp( 
-    MaterialApp( 
-      theme: ThemeData (
-        primarySwatch: Colors.deepOrange,
-        scaffoldBackgroundColor: Colors.blueGrey,
-      ),
-      initialRoute: '/',
-      routes: {
-      '/': (context) => const HomeRoute(),
-      '/second': (context) => const CampusMap(),
-      '/third': (context) => const EmergencyButton(),
-      },
-    )
-  ); 
+  runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp( 
+       theme: ThemeData (
+         primarySwatch: Colors.deepOrange,
+         scaffoldBackgroundColor: Colors.blueGrey,
+       ),
+      debugShowCheckedModeBanner: false,
+      home : HomePage(),
+    );
+  }
+}
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+  
+
+class _HomePageState extends State<HomePage> {
+  int pageIndex = 0;
+
+  final pages = [
+    const HomeRoute(),
+    const CampusMap(),
+    const EmergencyButton(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+        leading: Icon(
+          Icons.menu,
+          color: Theme.of(context).primaryColor,
+        ),
+        title: Text(
+          "Jacob's Part",
+          style: TextStyle(
+            color: Theme.of(context).primaryColor,
+            fontSize: 25,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+      ),
+      body: pages[pageIndex],
+      bottomNavigationBar: buildMyNavBar(context),
+    );
+  }
+
+  Container buildMyNavBar(BuildContext context) {
+    return Container(
+          height: 60,
+          decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+          ),
+
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+
+              IconButton(
+                enableFeedback: false,
+                onPressed: () {
+                  setState(() {
+                    pageIndex = 0;
+                  });
+                },
+                icon: pageIndex == 0
+                    ? const Icon(
+                        Icons.home_filled,
+                        color: Colors.white,
+                        size: 35,
+                      )
+                    : const Icon(
+                        Icons.home_outlined,
+                        color: Colors.white,
+                        size: 35,
+                      ),
+              ),
+
+              IconButton(
+                enableFeedback: false,
+                onPressed: () {
+                  setState(() {
+                    pageIndex = 1;
+                  });
+                },
+                icon: pageIndex == 1
+                    ? const Icon(
+                        Icons.work_rounded,
+                        color: Colors.white,
+                        size: 35,
+                      )
+                    : const Icon(
+                        Icons.work_outline_outlined,
+                        color: Colors.white,
+                        size: 35,
+                      ),
+              ),
+
+              IconButton(
+                enableFeedback: false,
+                onPressed: () {
+                  setState(() {
+                    pageIndex = 2;
+                  });
+                },
+                icon: pageIndex == 2
+                    ? const Icon(
+                        Icons.work_rounded,
+                        color: Colors.white,
+                        size: 35,
+                      )
+                    : const Icon(
+                        Icons.work_outline_outlined,
+                        color: Colors.white,
+                        size: 35,
+                      ),
+              ),
+            ],
+        ),
+    );
+  }
+}
 
 
 class HomeRoute extends StatelessWidget {
-const HomeRoute({Key? key}) : super(key: key);
+  const HomeRoute({Key? key}) : super(key: key);
 
-@override
-Widget build(BuildContext context) {
-	return Scaffold(
-	appBar: AppBar(
-		title: const Text('Jacobs Part'),
-		//backgroundColor: Color.fromARGB(255, 215, 116, 11),
-	), // AppBar
-	body: Center(
-		child: Column(
-		mainAxisAlignment: MainAxisAlignment.center,
-		children: <Widget>[
-			Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-              child: const Text('Campus Map'),
-              onPressed: () {
-                Navigator.pushNamed(context, '/second');
-              },
-              ),
-            ), // ElevatedButton
-			Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-              child: const Text('Emergency Button'),
-              onPressed: () {
-                Navigator.pushNamed(context, '/third');
-              },
-              ),
-            ), // ElevatedButton
-		], 
-		), 
-	), 
-	); 
-}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Temporary Homepage'),
+        //backgroundColor: Color.fromARGB(255, 215, 116, 11),
+      ), // AppBar
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                    child: const Text('Campus Map'),
+                      onPressed: () {
+                      },
+                    ),
+                  ), // ElevatedButton
+            Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                    child: const Text('Emergency Button'),
+                      onPressed: () {
+                      },
+                    ),
+                  ), // ElevatedButton
+          ], 
+        ), 
+      ), 
+    ); 
+  }
 }
 
 
 
 class CampusMap extends StatelessWidget {
-const CampusMap({Key? key}) : super(key: key);
+  const CampusMap({Key? key}) : super(key: key);
 
-@override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(
-      title: const Text("Campus Map"),
-      //backgroundColor: Color.fromARGB(255, 215, 116, 11),
-    ), // AppBar
-    body: Center(
-      child: Column (
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Text(
-              'Below find a detailed map of campus as well as the campus emergency stations:',
-              textAlign: TextAlign.center,
+
+      appBar: AppBar(
+        title: const Text("Campus Map"),
+        //backgroundColor: Color.fromARGB(255, 215, 116, 11),
+      ), // AppBar
+
+      body: Center(
+        child: Column (
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                'Below find a detailed map of campus as well as the campus emergency stations:',
+                textAlign: TextAlign.center,
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Back!'),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Back!'),
+              ),
             ),
-          ),
-          Image.asset(
-            'assets/campus_map.png',
-            width: 200,
-            height: 200,
-          ),
-        ],
-      )
-    ), 
+            Image.asset(
+              'assets/campus_map.png',
+              width: 200,
+              height: 200,
+            ),
+          ],
+        )
+      ), 
     ); 
   }
 }
@@ -107,6 +231,7 @@ const CampusMap({Key? key}) : super(key: key);
 
 makingCall() async {
   var url = Uri.parse("tel:6785164182");
+
   if (await canLaunchUrl(url)) {
     await launchUrl(url);
   } else {
@@ -117,9 +242,9 @@ makingCall() async {
 
 
 class EmergencyButton extends StatelessWidget {
-const EmergencyButton({Key? key}) : super(key: key);
+  const EmergencyButton({Key? key}) : super(key: key);
 
-@override
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
@@ -127,9 +252,11 @@ const EmergencyButton({Key? key}) : super(key: key);
           title: const Text('Emergency Call Button'),
           //backgroundColor: Color.fromARGB(255, 215, 116, 11),
         ), // AppBar
+
         body: SafeArea(
           child: Center(
             child: Column(
+
               children: [
                 Container(
                   height: 250.0,

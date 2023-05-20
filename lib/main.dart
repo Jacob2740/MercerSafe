c5a9aa2fa040b3644bd679d473f6ccab4b1184c4
LocketteDev <taiyahlock@gmail.com>
2023-05-20 16:26:04 -0400

    added main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+//IMPORTS///////////////////////////////////////////////////////////////////////
 import 'package:flutter/material.dart';
 import 'colors_mercer_official.dart';
 import 'home_screen.dart';
@@ -9,42 +10,55 @@ import 'package:flutter/services.dart';
 import 'report_incident.dart';
 import 'registry.dart';
 
+//FUNCTIONS/////////////////////////////////////////////////////////////////////
+//runs the app
 void main() => runApp(const MyApp());
 
+//CLASSES///////////////////////////////////////////////////////////////////////
 class MyApp extends StatelessWidget {
+  //constructor
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    //stops the app from rotating
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
 
     return MaterialApp(
+
+      //set the theme for the entire app.
         theme: ThemeData(
             primarySwatch: mercerOrange, scaffoldBackgroundColor: mercerBlack),
-        initialRoute: 'home',
+        initialRoute: 'home', //set the app to start on the home page
         onGenerateRoute: (settings) {
-          switch (settings.name) {
+          switch (settings.name) { //set the possible routes
             case 'home':
               return MaterialPageRoute(
                   builder: (context) => const MyHomePage());
+
             case 'genSafetyInfo':
               return MaterialPageRoute(
                   builder: (context) => const GenSafetyInfo());
+
             case 'safeJourney':
               return MaterialPageRoute(
                   builder: (context) => const SafeJourneyButton());
+
             case 'campusMap':
               return MaterialPageRoute(
                   builder: (context) => const CampusMap());
+
             case 'callButton':
               return MaterialPageRoute(
                   builder: (context) => const CallButton());
+
             case 'registry':
               return MaterialPageRoute(
                   builder: (context) =>  const RegistryView() );
+
             case 'reportIncident':
               return MaterialPageRoute(
                   builder: (context) => const ReportIncidentFeature(),
@@ -55,3 +69,5 @@ class MyApp extends StatelessWidget {
       );
   } 
 }
+
+//END OF main.dart//////////////////////////////////////////////////////////////
